f7ab576999ce1cfc4e8ff630df2d959799fa25f8
ZloyHR <dobrovolskyivan2002@gmail.com>
2021-03-23 12:18:38 +0200

    Added menu for searching, sorting and other options Implemented flow in search query
    Part6

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -1,18 +1,23 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
 import com.codinginflow.mvvmtodo.databinding.FragmentTaskListBinding
+import com.codinginflow.mvvmtodo.utils.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_task_list) {
 
-    private val viewModel : TasksViewModel by viewModels()
+    private val viewModel: TasksViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,8 +33,42 @@ class TasksFragment : Fragment(R.layout.fragment_task_list) {
             }
         }
 
-        viewModel.tasks.observe(viewLifecycleOwner){
+        viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_task_list, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.query.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+
+                true
+            }
+            R.id.action_delete_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -4,7 +4,16 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 
 class TasksViewModel @ViewModelInject constructor(private val taskDao: TaskDao) : ViewModel() {
-    val tasks = taskDao.getTasks().asLiveData()
+
+    val query = MutableStateFlow("")
+
+    private val taskFlow = query.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val tasks = taskFlow.asLiveData()
 }
